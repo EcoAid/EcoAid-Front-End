f91abf3df2753ef52face15891af3228827c89b3
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-06-27 15:50:26 -0300

    Merge arrumado

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    colors:{
-      transparent: 'transparent',
-      current: 'currentColor',
+    extend: {colors:{
       'ferngreen': '#407C44',
       'onyx': '#414141',
       'silver': '#A7A7A7',
       'isabelline': '#F7F3ED',
       'violetblue': '#3D4DA6',
-    }
+    }},
   },
   plugins: [],
 }
