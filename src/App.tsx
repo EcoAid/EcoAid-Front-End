d67b5d9bc2303fc3093072c1aa20c68410d7dad1
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-06-27 14:00:34 -0300

    Adicionado responsividade no home

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Home from './paginas/home/Home'
 import Sobre from './paginas/sobre/Sobre'
 
 function App() {
   return (
+    <>
+    <Home/>
     <Sobre/>
+    </>
   )
 }
 
--- src/paginas/home/home.tsx
+++ src/paginas/home/home.tsx
@@ -6,7 +6,7 @@ function home() {
     return (
         <>
             <main>
-                <div className='w-full p-24 text-6xl text-[#333333] bg-white'>
+                <div className='w-full p-24 text-6xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-[#333333] bg-white'>
                     <h1 className='w-2/5'><span className='text-[#407C44]'>Bom dia pessoa x,</span> gostaria de receber ou enviar doações</h1>
                 </div>
                 <div className='w-full p-12 flex flex-row justify-center gap-8'>
@@ -17,17 +17,17 @@ function home() {
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                 </svg>
                             </div>
-                            <input type="search" id="default-search" className="block w-full p-8 ps-24 text-2xl text-[#414141] rounded-3xl bg-white placeholder-[#414141]" placeholder="Procure por doações" required />
+                            <input type="search" id="default-search" className="block w-full p-8 ps-24 text-2xl sm:text-xl text-[#414141] rounded-3xl bg-white placeholder-[#414141]" placeholder="Procure por doações" required />
                         </div>
                     </form>
                     <button><SlidersHorizontal size={64} color='#407C44' /></button>
                 </div>
 
-                <section className='w-full part-white text-[#407C44] p-20 text-6xl flex flex-col gap-16 overflow-visible'>
+                <section className='w-full part-white text-[#407C44] p-20 text-6xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl flex flex-col gap-16 overflow-visible'>
                     <h1>Doações recomendadas</h1>
-                    <h2 className='text-4xl text-[#3D4DA6]'>Novas doações</h2>
+                    <h2 className='text-4xl sm:text-2xl md:text-3xl lg:text-4xl text-[#3D4DA6]'>Novas doações</h2>
                     <div className='flex flex-row gap-24'>
-                        <div className='bg-white w-2/7 shadow-2xl flex flex-col gap-4 justify-center box-border p-12 px-24 rounded-3xl text-center'>
+                        <div className='bg-white w-2/7 sm:w-1/7 lg:w-2/7 shadow-2xl flex flex-col gap-4 justify-center box-border p-12 px-24 rounded-3xl text-center'>
                             <Heart className='ml-auto' size={64} color="#407C44" />
                             <img className='rounded-full' src="https://cdn.betterttv.net/emote/658f28f95e7e78960777148d/3x.webp" alt="PlaceHolder" />
                             <h1 className='text-[#414141]'><span>Cesta básica</span></h1>
@@ -36,7 +36,7 @@ function home() {
                                 <button className='rounded-full bg-[#F7F3ED] p-6'><Plus size={32} color="#407C44" /></button>
                             </div>
                         </div>
-                        <div className='bg-white w-2/7 shadow-2xl flex flex-col gap-4 justify-center box-border p-12 px-24 rounded-3xl text-center'>
+                        <div className='bg-white w-2/7 sm:w-1/7 lg:w-2/7 shadow-2xl flex flex-col gap-4 justify-center box-border p-12 px-24 rounded-3xl text-center'>
                             <Heart className='ml-auto' size={64} color="#407C44" />
                             <img className='rounded-full' src="https://cdn.betterttv.net/emote/658f28f95e7e78960777148d/3x.webp" alt="PlaceHolder" />
                             <h1 className='text-[#414141]'><span>Cesta básica</span></h1>
@@ -45,7 +45,7 @@ function home() {
                                 <button className='rounded-full bg-[#F7F3ED] p-6'><Plus size={32} color="#407C44" /></button>
                             </div>
                         </div>
-                        <div className='bg-white w-2/7 shadow-2xl flex flex-col gap-4 justify-center box-border p-12 px-24 rounded-3xl text-center'>
+                        <div className='bg-white w-2/7 sm:w-1/7 lg:w-2/7 shadow-2xl flex flex-col gap-4 justify-center box-border p-12 px-24 rounded-3xl text-center'>
                             <Heart className='ml-auto' size={64} color="#407C44" />
                             <img className='rounded-full' src="https://cdn.betterttv.net/emote/658f28f95e7e78960777148d/3x.webp" alt="PlaceHolder" />
                             <h1 className='text-[#414141]'><span>Cesta básica</span></h1>
@@ -57,14 +57,14 @@ function home() {
                     </div>
                 </section>
 
-                <section className='w-full part-white text-[#407C44] p-20 text-6xl flex flex-col gap-16 overflow-visible'>
+                <section className='w-full part-white text-[#407C44] p-20 text-6xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl flex flex-col gap-16 overflow-visible'>
                     <div className='flex flex-row items-center gap-16'>
                         <h1>Minhas doações</h1>
-                        <button className='text-4xl text-[#3D4DA6]'>Mais populares</button>
-                        <button className='text-4xl text-[#3D4DA6]'>Top doações</button>
+                        <button className='text-4xl md:text-3xl sm:text-2xl text-[#3D4DA6]'>Mais populares</button>
+                        <button className='text-4xl md:text-3xl sm:text-2xl text-[#3D4DA6]'>Top doações</button>
                     </div>
                     <div className='flex flex-row gap-24'>
-                        <div className='bg-white w-2/7 shadow-2xl flex flex-col gap-4 justify-center box-border p-12 px-24 rounded-3xl text-center'>
+                    <div className='bg-white w-2/7 sm:w-1/7 lg:w-2/7 shadow-2xl flex flex-col gap-4 justify-center box-border p-12 px-24 rounded-3xl text-center'>
                             <Heart className='ml-auto' size={64} color="#407C44" />
                             <img className='rounded-full' src="https://cdn.betterttv.net/emote/658f28f95e7e78960777148d/3x.webp" alt="PlaceHolder" />
                             <h1 className='text-[#414141]'><span>Cesta básica</span></h1>
@@ -73,7 +73,7 @@ function home() {
                                 <button className='rounded-full bg-[#F7F3ED] p-6'><Plus size={32} color="#407C44" /></button>
                             </div>
                         </div>
-                        <div className='bg-white w-2/7 shadow-2xl flex flex-col gap-4 justify-center box-border p-12 px-24 rounded-3xl text-center'>
+                        <div className='bg-white w-2/7 sm:w-1/7 lg:w-2/7 shadow-2xl flex flex-col gap-4 justify-center box-border p-12 px-24 rounded-3xl text-center'>
                             <Heart className='ml-auto' size={64} color="#407C44" />
                             <img className='rounded-full' src="https://cdn.betterttv.net/emote/658f28f95e7e78960777148d/3x.webp" alt="PlaceHolder" />
                             <h1 className='text-[#414141]'><span>Cesta básica</span></h1>
@@ -82,7 +82,7 @@ function home() {
                                 <button className='rounded-full bg-[#F7F3ED] p-6'><Plus size={32} color="#407C44" /></button>
                             </div>
                         </div>
-                        <div className='bg-white w-2/7 shadow-2xl flex flex-col gap-4 justify-center box-border p-12 px-24 rounded-3xl text-center'>
+                        <div className='bg-white w-2/7 sm:w-1/7 lg:w-2/7 shadow-2xl flex flex-col gap-4 justify-center box-border p-12 px-24 rounded-3xl text-center'>
                             <Heart className='ml-auto' size={64} color="#407C44" />
                             <img className='rounded-full' src="https://cdn.betterttv.net/emote/658f28f95e7e78960777148d/3x.webp" alt="PlaceHolder" />
                             <h1 className='text-[#414141]'><span>Cesta básica</span></h1>
