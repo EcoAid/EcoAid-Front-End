1faaa8f90647568ae544ab3aafd7da3189d47c36
Daniel Saburo Akiyama <danielakiyama8@gmail.com>
2024-07-12 17:39:18 -0300

    Feat: Deploy

--- src/components/carrinho/Carrinho.tsx
+++ src/components/carrinho/Carrinho.tsx
@@ -20,7 +20,6 @@ function Carrinho() {
             <MenuItems
                 modal={false}
                 transition
-                onClick={(e) => { e.preventDefault() }}
                 className="overflow-hidden absolute right-0 z-10 mt-2 w-[550px] p-4 origin-top-right divide-y rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
             >
                 <div className='flex flex-col gap-4 rounded-md'>
@@ -35,7 +34,7 @@ function Carrinho() {
                         <MenuItem key={produto.id} as={Fragment}>
 
                             {({ focus }) => (
-                                <div className={clsx('text-sm py-2 px-4 text-nowrap flex gap-8 block', focus && 'bg-green-100')}>
+                                <div onClick={(e) => { e.preventDefault() }} className={clsx('text-sm py-2 px-4 text-nowrap flex gap-8 block', focus && 'bg-green-100')}>
                                     <p className='w-52 text-wrap line-clamp-1 break-words'> {produto.nome} </p>
                                     <button onClick={(e) => { adicionarItem(produto); e.preventDefault() }}><CaretUp /></button>
                                     <p>Quantidade: {produto.qtd}</p>
@@ -46,7 +45,7 @@ function Carrinho() {
                         </MenuItem>
                     ))}
                     <MenuItem>
-                    <div className="text-sm flex justify-between border-t-2 pt-4">
+                    <div onClick={(e) => { e.preventDefault() }} className="text-sm flex justify-between border-t-2 pt-4">
                     <div className='w-full flex items-center text-sm justify-between'>
                         <button className='flex items-center w-fit h-12 rounded-md transition hover:text-ferngreen hover:bg-transparent border border-green-800 text-white bg-ferngreen p-4' onClick={() => finalizarCompra()}> Finalizar </button>
                         <p className="text-lg">Total: {new Intl.NumberFormat('pt-BR', {
