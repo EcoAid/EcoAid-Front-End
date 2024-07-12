23cdc7749b416b204a73bda09890089e79abcf41
Desyrre Barbosa <ddesyrre@icloud.com>
2024-07-12 14:13:33 -0300

    Atualizações Footer, Navbar e Sobre

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -51,15 +51,15 @@ function Navbar() {
       <div className='w-full bg-white text-[#407C44] flex justify-center py-4 border-b-2 border-gray-200'>
         <div className="container flex justify-between text-lg ">
           <div className='flex items-center'>
-          <Link to='/home'><h1 className='text-2xl font-bold uppercase bg-isabelline'>EcoAid</h1></Link>
+          <Link to='/home'><h1 className='text-2xl font-bold uppercase bg-isabelline  hover:text-onyx transition ease-in-out'>EcoAid</h1></Link>
           </div>
 
           <div className='flex items-center gap-8'>
-            <Link to='/sobre' className='hover:underline'>Sobre nós</Link>
-            <Link to='/doacoes' className='hover:underline'>Doações</Link>
-            <Link to='/categorias' className='hover:underline'>Categorias</Link>
-            <Link to='/home' className='hover:underline'>Devoluções e Cancelamentos</Link>
-            <Link to='/home' className='hover:underline font-bold'><TiShoppingCart size={38} /></Link>
+            <Link to='/sobre' className='hover:underline hover:text-onyx  transition ease-in-out'>Sobre nós</Link>
+            <Link to='/doacoes' className='hover:underline hover:text-onyx  transition ease-in-out'>Doações</Link>
+            <Link to='/categorias' className='hover:underline hover:text-onyx transition ease-in-out'>Categorias</Link>
+            <Link to='/home' className='hover:underline hover:text-onyx  transition ease-in-out'>Devoluções e Cancelamentos</Link>
+            <Link to='/home' className='hover:text-onyx transition ease-in-out font-bold'><TiShoppingCart size={38} /></Link>
             {usuarioProfile}
           </div>
         </div>
